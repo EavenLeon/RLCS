3966c553ead689e146939c1a28b77cb0fdf21ab4
Leon <ywhuak@163.com>
2018-05-03 15:21:29 +0800

    test

--- src/main/java/IWSRC/RLCC/A.java
+++ src/main/java/IWSRC/RLCC/A.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Field;
  * @Description:  
  */
 public class A {
-	public static void main(String[] args) {
+	public static void my() {
 		Lab lab = new Lab();
         for(Field field : lab.getClass().getDeclaredFields()){  
             if(field.isAnnotationPresent(ST.class)){  //如果存在MyTag标签  
---
+++ src/main/java/IWSRC/RLCC/aa.java
@@ -0,0 +1,17 @@
+package IWSRC.RLCC;
+
+import org.junit.Test;
+
+/** 
+ * @author: Yiwen Liang[ywhuak@163.com]
+ * @date: Created on 14:52 2018/5/3 
+ * @Description:  
+ */
+public class aa {
+
+	@Test
+	public void test() {
+		A.my();;
+	}
+
+}
